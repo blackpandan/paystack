02319e788edf85521a2ac87d27066f8795f7e0e5
Faith_James <faithgstc54@gmail.com>
2022-09-01 23:55:21 +0100

    Update apps.py

--- paystack/apps.py
+++ paystack/apps.py
@@ -6,5 +6,4 @@ CURRENT_DIRECTOR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 class PaystackConfig(AppConfig):
     name = "paystack"
-    path = os.path.join(CURRENT_DIRECTOR, "django")
 
