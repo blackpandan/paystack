67478fa1c82d4fc8d38cac98223cec63862808ea
blackpandan <faithgstc54@gmail.com>
2022-09-02 00:07:21 +0100

    added app path in apps.py path

--- paystack/app/apps.py
+++ paystack/app/apps.py
@@ -1,9 +1,9 @@
 import os
 from django.apps import AppConfig
 
-CURRENT_DIRECTOR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CURRENT_DIRECTORY = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 class PaystackConfig(AppConfig):
     name = "paystack"
-
+    path = os.path.join(CURRENT_DIRECTORY, "app")
